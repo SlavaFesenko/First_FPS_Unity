f584a665a1c1b71ff64620e664f8c672ac88dc4a
Viacheslav Fesenko <vfesenko@deloitte.com>
2018-12-19 11:27:41 +0200

    move/dead enemy

--- CustomFPS/Assets/ReactiveTarget.cs
+++ CustomFPS/Assets/ReactiveTarget.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class ReactiveTarget : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void ReactToHit()
+    {
+        WanderingAI behavior = GetComponent<WanderingAI>();
+        if (behavior != null)
+        {
+            behavior.SetAlive(false);
+        }
+        StartCoroutine(Die());
+    }
+
+    private IEnumerator Die()
+    {
+        Vector3 fallVector = new Vector3(-75, 0, 0);
+        this.transform.Rotate(fallVector);
+
+        yield return new WaitForSeconds(1.5f);
+
+        Destroy(this.gameObject);
+    }
 }
